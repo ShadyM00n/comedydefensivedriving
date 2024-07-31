ba819a9bc726b9c3043971f1dbcfd6180cce319e
ShadyMoon <103329255+ShadyM00n@users.noreply.github.com>
2024-07-31 11:32:55 -0400

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ this may work for other sites but as i havent tested, i cannot be sure.
 const axios = require('axios'); const qs = require('qs');
 // dashboard link
 const referer = 'https://comedydefensivedriving.com/attender/dashboard.php';
-// Cookie contents # dont change
+// Cookie contents # dont change after setting
 const cookie = `your_cookie_content`;
 // Headers # dont change
 const headers = { 'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7', 'accept-language': 'en-US,en;q=0.9', 'cache-control': 'max-age=0', 'sec-fetch-dest': 'document', 'sec-fetch-mode': 'navigate', 'sec-fetch-site': 'same-origin', 'upgrade-insecure-requests': '1', 'cookie': cookie,'Referer': referer, 'Referrer-Policy': 'strict-origin-when-cross-origin' };
